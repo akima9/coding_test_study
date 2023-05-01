a338c92341a4dff33fdc02bc4a35f6603f35e3a4
devGiYong <akima9a@gmail.com>
2023-05-01 11:15:29 +0900

    마구간 정하기(결정알고리즘)
    Signed-off-by: devGiYong <akima9a@gmail.com>

--- src/main/java/codingTestStudy/ex06_10.java
+++ src/main/java/codingTestStudy/ex06_10.java
@@ -6,27 +6,60 @@ import java.util.Scanner;
 public class ex06_10 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-//        int N = in.nextInt();
-//        int M = in.nextInt();
-//        int[] points = new int[N];
-//        for (int i = 0; i < N; i++) {
-//            points[i] = in.nextInt();
-//        }
-
-        int N = 5;
-        int M = 3;
-        int[] points = {1, 2, 8, 4, 9};
+        int N = in.nextInt();
+        int M = in.nextInt();
+        int[] points = new int[N];
+        for (int i = 0; i < N; i++) {
+            points[i] = in.nextInt();
+        }
         System.out.println(solution(N, M, points));
     }
 
+    /**
+     * 가장 가까운 두 말의 최대 거리를 반환한다.
+     * @param N 마구간의 수
+     * @param M 말의 수
+     * @param points 마구간 좌표
+     * @return 가장 가까운 두 말의 최대 거리
+     */
     private static int solution(int N, int M, int[] points) {
-        System.out.println("N = " + N);
-        System.out.println("M = " + M);
-        System.out.println("Arrays.toString(points) = " + Arrays.toString(points));
-        
-        return 0;
+        int result = 0;
+
+        // 마구간 좌표 오름차순 정렬
+        Arrays.sort(points);
+        // 두 말 사이의 거리 중 최소가 될 수 있는 값
+        int min = 1;
+        // 두 말 사이의 거리 중 최대가 될 수 있는 값
+        int max = points[N - 1] - 1;
+        // min ~ max 범위를 이분 탐색
+        while (min <= max) {
+            // min ~ max의 중간 값
+            int mid = (min + max) / 2;
+            // 이전 말의 위치
+            int prev = 0;
+            // 말의 수
+            int horseCnt = 1;
+            for (int i = 0; i < N; i++) {
+                if (i == 0) {
+                    prev = points[i];
+                } else {
+                    // 이전 말의 좌표와 현재 말의 좌표 사이의 거리가 mid 보다 크거나 같은 경우
+                    if (points[i] - prev >= mid) {
+                        prev = points[i];
+                        horseCnt++;
+                    }
+                }
+            }
+
+            if (horseCnt >= M) {
+                min = mid + 1;
+                result = mid;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return result;
     }
-    // 1, 2, 4, 8, 9
 }
 //10. 마구간 정하기(결정알고리즘)
 //설명
