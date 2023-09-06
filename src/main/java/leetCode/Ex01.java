52419fedec12a1b173f7922b86e2578a255c6306
devGiYong <akima9a@gmail.com>
2023-09-06 20:30:46 +0900

    leetcode: Binary Tree Inorder Traversal
    Signed-off-by: devGiYong <akima9a@gmail.com>

---
+++ src/main/java/leetCode/Ex01.java
@@ -0,0 +1,45 @@
+package leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+public class Ex01 {
+    public static void main(String[] args) {
+        TreeNode left = new TreeNode(3);
+        TreeNode right = new TreeNode(2, left, null);
+        TreeNode root = new TreeNode(1, null, right);
+        System.out.println(inorderTraversal(root));
+    }
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+        DFS(root, answer);
+        return answer;
+    }
+
+    public static void DFS(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        else {
+            DFS(node.left, list);
+            list.add(node.val);
+            DFS(node.right, list);
+        }
+    }
+}
