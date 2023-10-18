605db383fa7cf6b892a1074a7a31761cdde8cc6f
devGiYong <akima9a@gmail.com>
2023-10-18 17:51:11 +0900

    :hammer: 불
    Signed-off-by: devGiYong <akima9a@gmail.com>

--- src/main/java/BOJ/Ex4.java
+++ src/main/java/BOJ/Ex4.java
@@ -10,6 +10,12 @@ import java.util.StringTokenizer;
 public class Ex4 {
     static int[] dx = {1, 0, -1, 0};
     static int[] dy = {0, 1, 0, -1};
+
+    /**
+     * 불이 퍼지는 속도를 BFS로 구하고,
+     * 사람이 이동하는 속도를 BFS로 구한다.
+     * 단, 사람은 같은 장소를 불보다 나중에 오거나 동시에 올 수 없다.
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,9 +36,6 @@ public class Ex4 {
                 } else if (map[i][j] == 'F') {
                     fire.offer(new int[]{i, j});
                     burn[i][j] = 1;
-                } else if (map[i][j] == '#') {
-                    visit[i][j] = -1;
-                    burn[i][j] = -1;
                 }
             }
         }
@@ -43,11 +46,10 @@ public class Ex4 {
                 int[] curPoint = fire.poll();
                 int x = curPoint[0];
                 int y = curPoint[1];
-
                 for (int j = 0; j < 4; j++) {
                     int nx = x + dx[j];
                     int ny = y + dy[j];
-                    if (nx < 0 || ny < 0 || nx >= R || ny >= C || burn[nx][ny] != 0) continue;
+                    if (nx < 0 || ny < 0 || nx >= R || ny >= C || map[nx][ny] == '#' || burn[nx][ny] != 0) continue;
                     burn[nx][ny] = burn[x][y] + 1;
                     fire.offer(new int[]{nx, ny});
                 }
@@ -60,7 +62,6 @@ public class Ex4 {
                 int[] curPoint = person.poll();
                 int x = curPoint[0];
                 int y = curPoint[1];
-
                 for (int j = 0; j < 4; j++) {
                     int nx = x + dx[j];
                     int ny = y + dy[j];
@@ -70,7 +71,7 @@ public class Ex4 {
                         return;
                     }
 
-                    if (burn[nx][ny] <= visit[x][y] + 1 || visit[nx][ny] != 0) continue;
+                    if (visit[nx][ny] != 0 || map[nx][ny] == '#' || visit[x][y] + 1 >= burn[nx][ny] && burn[nx][ny] != 0) continue;
                     visit[nx][ny] = visit[x][y] + 1;
                     person.offer(new int[]{nx, ny});
                 }
