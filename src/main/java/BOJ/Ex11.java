bf56a40bea68e70a810853b51a7ecbd1b643fb1f
devGiYong <akima9a@gmail.com>
2023-10-26 19:59:06 +0900

    :hammer: 벽 부수고 이동하기
    Signed-off-by: devGiYong <akima9a@gmail.com>

--- src/main/java/BOJ/Ex11.java
+++ src/main/java/BOJ/Ex11.java
@@ -8,13 +8,13 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Ex11 {
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int[] dx = {1, 0, -1, 0}; // 상하좌우 방향 이동을 위한 x 좌표 변화
+    static int[] dy = {0, 1, 0, -1}; // 상하좌우 방향 이동을 위한 y 좌표 변화
 
     private static class Point {
         int x;
         int y;
-        int isDestroy;
+        int isDestroy; // 벽을 부수었는지 여부를 나타내는 변수
 
         public Point(int x, int y, int isDestroy) {
             this.x = x;
@@ -22,13 +22,15 @@ public class Ex11 {
             this.isDestroy = isDestroy;
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); //열
-        int M = Integer.parseInt(st.nextToken()); //행
-        int[][] map = new int[N][M]; //미로
-        int[][][] visit = new int[2][N][M]; //방문표시
+        int N = Integer.parseInt(st.nextToken()); // 열
+        int M = Integer.parseInt(st.nextToken()); // 행
+        int[][] map = new int[N][M]; // 미로
+        int[][][] dist = new int[N][M][2]; // 거리 측정 (벽을 부셨을 때/안 부셨을 때)
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             String row = st.nextToken();
@@ -36,51 +38,50 @@ public class Ex11 {
                 map[i][j] = Character.getNumericValue(row.charAt(j));
             }
         }
+
         Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(0, 0, 0));
-        visit[0][0][0] = 1;
-        visit[1][0][0] = 1;
+        queue.offer(new Point(0, 0, 0)); // 시작점을 큐에 추가하고 벽을 부수지 않았으므로 isDestroy를 0으로 설정
+        dist[0][0][0] = 1; // 시작점의 거리를 1로 설정
+
         while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Point curPoint = queue.poll();
-                int x = curPoint.x;
-                int y = curPoint.y;
+            Point curPoint = queue.poll();
+            int x = curPoint.x;
+            int y = curPoint.y;
+            int isDestroy = curPoint.isDestroy;
 
-                for (int j = 0; j < 4; j++) {
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
+            // 상하좌우로 이동
+            for (int j = 0; j < 4; j++) {
+                int nx = x + dx[j];
+                int ny = y + dy[j];
 
-                    if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                // 경계를 벗어나지 않도록 체크
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
 
-                    //visit[0] 그냥 통과
-                    //visit[1] 부시고 통과
+                // 벽이 없고, 아직 방문하지 않았다면 거리를 업데이트하고 큐에 추가
+                if (map[nx][ny] == 0 && dist[nx][ny][isDestroy] == 0) {
+                    dist[nx][ny][isDestroy] = dist[x][y][isDestroy] + 1;
+                    queue.offer(new Point(nx, ny, isDestroy));
+                }
 
-                    if (map[nx][ny] == 1) { //벽을 만나는 경우.
-                        if (curPoint.isDestroy == 1) { //전에 부신적 있으면 통과.
-                            continue;
-                        } else { //전에 부신적 없는 경우.
-                            visit[1][nx][ny] = visit[0][x][y] + 1;
-                            queue.offer(new Point(nx, ny, 1));
-                        }
-                    } else { //벽이 아닌 경우
-                        if (visit[curPoint.isDestroy][nx][ny] >= 1) continue; //방문 한적 있으면 통과.
-                        visit[curPoint.isDestroy][nx][ny] = visit[curPoint.isDestroy][x][y] + 1;
-                        queue.offer(new Point(nx, ny, curPoint.isDestroy));
-                    }
-
-                    if (nx == N - 1 && ny == M - 1) {
-                        int min = Math.min(visit[0][nx][ny], visit[1][nx][ny]);
-                        if (min == 0) {
-                            System.out.println(Math.max(visit[0][nx][ny], visit[1][nx][ny]));
-                        } else {
-                            System.out.println(min);
-                        }
-                        return;
-                    }
+                // 벽을 부수지 않았고 벽을 만나면 벽을 부순 경우로 처리
+                if (isDestroy == 0 && map[nx][ny] == 1 && dist[nx][ny][1] == 0) {
+                    dist[nx][ny][1] = dist[x][y][0] + 1;
+                    queue.offer(new Point(nx, ny, 1));
                 }
             }
         }
-        System.out.println(-1);
+
+        int ans1 = dist[N - 1][M - 1][0]; // 벽을 부수지 않고 도착한 경우의 거리
+        int ans2 = dist[N - 1][M - 1][1]; // 벽을 부수고 도착한 경우의 거리
+
+        if (ans1 == 0 && ans2 == 0) {
+            System.out.println(-1); // 목적지에 도달할 수 없는 경우
+        } else if (ans1 == 0) {
+            System.out.println(ans2); // 벽을 부수고 도착한 경우만 가능한 경우
+        } else if (ans2 == 0) {
+            System.out.println(ans1); // 벽을 부수지 않고 도착한 경우만 가능한 경우
+        } else {
+            System.out.println(Math.min(ans1, ans2)); // 두 경우 중 더 작은 거리 출력
+        }
     }
 }
