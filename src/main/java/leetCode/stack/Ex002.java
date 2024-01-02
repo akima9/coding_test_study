f0208df2fd10e535757e930016d29f418cc83012
giyong <akima9a@gmail.com>
2024-01-02 18:33:52 +0900

    :hammer: Valid Parentheses
    Signed-off-by: giyong <akima9a@gmail.com>

---
+++ src/main/java/leetCode/stack/Ex002.java
@@ -0,0 +1,52 @@
+package leetCode.stack;
+
+import java.util.Stack;
+
+public class Ex002 {
+    public static void main(String[] args) {
+        System.out.println(isValid(")"));
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                if (stack.size() > 0 && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (c == '}') {
+                if (stack.size() > 0 && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (c == ']') {
+                if (stack.size() > 0 && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                stack.add(c);
+            }
+        }
+
+        if (stack.size() > 0) {
+            return false;
+        }
+        return true;
+    }
+}
+//Example 1:
+//
+//Input: s = "()"
+//Output: true
+//Example 2:
+//
+//Input: s = "()[]{}"
+//Output: true
+//Example 3:
+//
+//Input: s = "(]"
+//Output: false
